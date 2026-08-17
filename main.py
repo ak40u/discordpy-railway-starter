a911982f0440874d4c39f02c28566d5aa6dedff1
qoriaguard-creator <qoriaguard@gmail.com>
2026-08-17 16:23:05 +0400

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY")
 
 if GEMINI_API_KEY:
     genai.configure(api_key=GEMINI_API_KEY)
-    ai_model = genai.GenerativeModel("gemini-pro")
+    ai_model = genai.GenerativeModel("models/gemini-1.5-flash")
 else:
     ai_model = None
 
